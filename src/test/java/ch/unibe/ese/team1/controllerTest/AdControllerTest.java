b583d51ad6a64a74782a25964e14e6c398b30f6a
Simon Kafader <simon.kafader@students.unibe.ch>
2016-12-12 13:56:25 +0100

    updated junit version in pom and uploaded the AdControllerTest

---
+++ src/test/java/ch/unibe/ese/team1/controllerTest/AdControllerTest.java
@@ -0,0 +1,114 @@
+package ch.unibe.ese.team1.controllerTest;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.YamlProcessor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * Created by smoen on 12.12.2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/config/springMVC.xml",
+        "file:src/main/webapp/WEB-INF/config/springData.xml",
+        "file:src/main/webapp/WEB-INF/config/springSecurity.xml" })
+@WebAppConfiguration
+@Transactional
+public class AdControllerTest {
+
+    private MockMvc mockMvc;
+   // private Principal principal;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void adNonExisting() throws Exception{
+        this.mockMvc.perform(get("/ad").param("id","97699")).andExpect(view().name("pageNotFound"));
+    }
+
+    @Test
+    public void messageSent() throws Exception{
+        this.mockMvc.perform(get("/ad").principal(getPrincipal("jane@doe.ch")).param("id","1")
+                .param("msgSubject","test").param("msgTextarea","jojo")).andExpect(view().name("adDescription"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void inquirySent() throws Exception{
+        this.mockMvc.perform(get("/ad").principal(getPrincipal("jane@doe.ch")).param("id","1")
+                .param("confirmationDialog","")).andExpect(status().isOk())
+                .andExpect(view().name("adDescription"));
+    }
+
+    @Test
+    public void watchProfile() throws Exception{
+        this.mockMvc.perform(get("/ad").principal(getPrincipal("jane@doe.ch")).param("id","1"));
+    }
+
+    @Test
+    public void isBookmarked() throws Exception{
+         MvcResult result = this.mockMvc.perform(post("/bookmark").principal(getPrincipal("jane@doe.com"))
+                    .param("id","1").param("screening","true").param("bookmarked","true"))
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+        assertEquals("2",result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void unbookmarkAd() throws Exception{
+        MvcResult result  = this.mockMvc.perform(post("/bookmark").principal(getPrincipal("ese@unibe.ch"))
+                .param("id","1").param("screening","true").param("bookmarked","true"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals("3",result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void bookmarkOwnAdandFail() throws Exception{
+        MvcResult result  = this.mockMvc.perform(post("/bookmark").principal(getPrincipal("user@bern.com"))
+                .param("id","1").param("screening","true").param("bookmarked","true"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals("4",result.getResponse().getContentAsString());
+    }
+
+
+    private Principal getPrincipal(String name){
+        Principal principal = new Principal() {
+            @Override
+            public String getName() {
+                return name;
+            }
+        };
+        return principal;
+    }
+
+
+
+
+}
