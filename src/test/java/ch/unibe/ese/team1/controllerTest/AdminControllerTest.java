6c65362a5c43fbdf9b6d2d3c17cc61d6f5c54035
Dominik Briner <dominik.briner@students.unibe.ch>
2016-12-12 15:20:42 +0100

    Extended AuctionControllerTest. Added more tests.

---
+++ src/test/java/ch/unibe/ese/team1/controllerTest/AdminControllerTest.java
@@ -0,0 +1,66 @@
+package ch.unibe.ese.team1.controllerTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/config/springMVC.xml",
+        "file:src/main/webapp/WEB-INF/config/springData.xml",
+        "file:src/main/webapp/WEB-INF/config/springSecurity.xml" })
+public class AdminControllerTest {
+
+    @Autowired
+    WebApplicationContext wac;
+    @Autowired
+    MockHttpServletRequest request;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void insightsWorks() throws Exception {
+        MvcResult result  = this.mockMvc.perform(post("/admin/insights").principal(getPrincipal("System")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("insights"))
+                .andExpect(model().attributeExists("smokersCount", "animalsCount", "washingMachineCount",
+                        "apartmentCount", "expiredAdsCount", "adsCount", "usersCount", "currentUser", "moneySpent",
+                        "userName", "provisionsMade", "premiumAdMoney"))
+                .andReturn();
+        assertEquals(result.getModelAndView().getModel().)
+    }
+
+
+
+    private Principal getPrincipal(String name){
+        Principal principal = new Principal() {
+            @Override
+            public String getName() {
+                return name;
+            }
+        };
+        return principal;
+    }
+}
