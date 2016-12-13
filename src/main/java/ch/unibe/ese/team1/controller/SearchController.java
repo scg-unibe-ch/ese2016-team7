c4ede65d3a0470ba98698eb16a9e0e08ada27249
Jonas Richner <jonas.richner@students.unibe.ch>
2016-12-13 22:51:57 +0100

    Updated quicksearch parameters to better values. (decreased radius, increased price)

--- src/main/java/ch/unibe/ese/team1/controller/SearchController.java
+++ src/main/java/ch/unibe/ese/team1/controller/SearchController.java
@@ -52,8 +52,8 @@ public class SearchController {
 	public ModelAndView quicksearch(@RequestParam("city") String city){
 		receivedRequest("SearchController", "/quicksearch");
 		searchForm.setCity(city);
-		searchForm.setRadius(50);
-		searchForm.setPrice(2000000);
+		searchForm.setRadius(20);
+		searchForm.setPrice(1000000);
 		searchForm.setStudio(true);
 		searchForm.setHouse(true);
 		searchForm.setApartment(true);
